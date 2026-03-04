128d39079e844cce7b420c7b46cc495c38252fd3
nl66736-coder <nl66736@gmail.com>
2026-03-04 20:01:08 +0100

    Update voraz.py

--- voraz.py
+++ voraz.py
@@ -1,1 +1,32 @@
-voraz
+class BusquedaVoraz(Busqueda):
+    def buscarSolucion(self,inicial):
+        nodoActual = None
+        actual, hijo = None, None
+        solucion = False
+        abiertos = []
+        cerrados = dict()
+        abiertos.append(NodoVoraz(inicial, None, None))
+        cerrados[inicial.cubo.visualizar()]=inicial
+        while not solucion and len(abiertos)>0:
+            nodoActual = abiertos.pop(0)
+            actual = nodoActual.estado
+            if actual.esFinal():
+                solucion = True
+            else:
+                #cerrados[actual.cubo.visualizar()] = actual
+                for operador in actual.operadoresAplicables():
+                    hijo = actual.aplicarOperador(operador)
+                    if hijo.cubo.visualizar() not in cerrados.keys():
+                        abiertos.append(NodoVoraz(hijo, nodoActual, operador))
+                        cerrados[hijo.cubo.visualizar()] = hijo #utilizamos CERRADOS para mantener también traza de los nodos añadidos a ABIERTOS 
+                        abiertos.sort(key=lambda x: x.heuristica) #ordenamos ABIERTOS por heurística (menor primero)
+        if solucion:
+            lista = []
+            nodo = nodoActual
+            while nodo.padre != None: #Asciende hasta la raíz
+                lista.insert(0, nodo.operador)
+                nodo = nodo.padre
+            return lista
+        else:
+            return None
+
