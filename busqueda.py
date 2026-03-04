1bbe26eb2b8164c46848ac70c270ac24066af75b
nl66736-coder <nl66736@gmail.com>
2026-03-04 20:02:28 +0100

    Update busqueda.py

--- busqueda.py
+++ busqueda.py
@@ -58,7 +58,7 @@ class BusquedaProfundidad(Busqueda):
         solucion = False
         abiertos = []
         cerrados = dict()
-        abiertos.append(NodoAnchura(inicial, None, None))
+        abiertos.append(NodoProfundidad(inicial, None, None))
         cerrados[inicial.cubo.visualizar()]=inicial
         while not solucion and len(abiertos)>0:
             nodoActual = abiertos.pop()
@@ -72,6 +72,38 @@ class BusquedaProfundidad(Busqueda):
                     if hijo.cubo.visualizar() not in cerrados.keys():
                         abiertos.append(NodoAnchura(hijo, nodoActual, operador))
                         cerrados[hijo.cubo.visualizar()] = hijo #utilizamos CERRADOS para mantener también traza de los nodos añadidos a ABIERTOS 
+        if solucion:
+            lista = []
+            nodo = nodoActual
+            while nodo.padre != None: #Asciende hasta la raíz
+                lista.insert(0, nodo.operador)
+                nodo = nodo.padre
+            return lista
+        else:
+            return None
+
+class BusquedaVoraz(Busqueda):
+    def buscarSolucion(self,inicial):
+        nodoActual = None
+        actual, hijo = None, None
+        solucion = False
+        abiertos = []
+        cerrados = dict()
+        abiertos.append(NodoVoraz(inicial, None, None))
+        cerrados[inicial.cubo.visualizar()]=inicial
+        while not solucion and len(abiertos)>0:
+            nodoActual = abiertos.pop(0)
+            actual = nodoActual.estado
+            if actual.esFinal():
+                solucion = True
+            else:
+                #cerrados[actual.cubo.visualizar()] = actual
+                for operador in actual.operadoresAplicables():
+                    hijo = actual.aplicarOperador(operador)
+                    if hijo.cubo.visualizar() not in cerrados.keys():
+                        abiertos.append(NodoVoraz(hijo, nodoActual, operador))
+                        cerrados[hijo.cubo.visualizar()] = hijo #utilizamos CERRADOS para mantener también traza de los nodos añadidos a ABIERTOS 
+                        abiertos.sort(key=lambda x: x.heuristica) #ordenamos ABIERTOS por heurística (menor primero)
         if solucion:
             lista = []
             nodo = nodoActual
